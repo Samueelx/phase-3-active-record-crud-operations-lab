324bd8a2b41c43bebe858bf8f0a10cc5a82a8b02
Samueelx <gsammygitau@gmail.com>
2023-03-02 09:23:52 +0300

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(the_title)
+        movie = self.create(title: the_title)
+    end
 
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attribute)
+        self.find_by(attribute)
+    end
+
+    def self.find_movies_after_2002
+        self.where("release_date > 2002")
+    end
+
+    def update_with_attributes(title:)
+        self.title = title
+    end
+
+    def self.update_all_titles(title)
+        self.update(:all, title: title)
+    end
+
+    def self.delete_by_id(id)
+        movie = self.find(id)
+        movie.destroy
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
 end
